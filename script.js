e2fafc878af1226ce21bfb0a6be44ca5a3098280
beethfridge1 <rekameow@gmail.com>
2026-03-13 23:15:32 +0100

    Remove size and rotation from sparkle effect
    Removed sparkle size and rotation calculations.

--- script.js
+++ script.js
@@ -7,11 +7,6 @@ document.addEventListener("mousemove", function(e) {
   sparkle.style.top = e.clientY + "px";
 
   document.body.appendChild(sparkle);
-
-const size = Math.random() * 6 + 4;
-sparkle.style.width = size + "px";
-sparkle.style.height = size + "px";
-sparkle.style.transform = `rotate(${Math.random()*360}deg)`;
   
   setTimeout(() => {
     sparkle.remove();
