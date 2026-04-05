204276d1b41ea7ab9b59e1354ddd6f9759387a3c
beethfridge1 <rekameow@gmail.com>
2026-04-05 20:19:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let isImage = false;
 
 el.addEventListener("click", () => {
   if (!isImage) {
-    el.innerHTML = '<img src="kep1.jpg" style="height:40px;">';
+    el.innerHTML = '<img src="toas.png" style="height:40px;">';
   } else {
     el.textContent = 'r';
   }
